e9983f3de24fce7771a2928106ae1b6ada28ae84
Vikash Thakur <thakvikash622@gmail.com>
2024-08-18 00:49:11 +0530

    setting up the cleark

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       <h1>This is Home page</h1>
+      <UserButton  />
     </div>
   )
 }
 
 export default page
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const IMBPlex = IBM_Plex_Sans({ subsets: ["latin"], weight: ['400', '500', '600', '700'], variable: '--font-ibm-plex' });
 
@@ -19,7 +19,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={cn("font-IBMPlex antialiased", IMBPlex.variable)}>{children}</body>
+        <body className={cn("font-IBMPlex antialiased", IMBPlex.variable)}>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
