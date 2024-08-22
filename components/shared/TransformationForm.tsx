72d701433ad046c0aa15b65c965cec82f9f70e48
Vikash Thakur <thakvikash622@gmail.com>
2024-08-22 23:26:33 +0530

    Done with Add Image form

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -14,11 +14,11 @@ import {
 
 
 import { Form } from "@/components/ui/form"
-import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants"
+import { aspectRatioOptions, creditFee, defaultValues, transformationTypes } from "@/constants"
 import { CustomField } from "./CustomField"
 import { Input } from "../ui/input"
-import { useState } from "react"
-import { AspectRatioKey, debounce } from "@/lib/utils"
+import { useState, useTransition } from "react"
+import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
 import { Button } from "../ui/button"
 
 // will be the schema of the form 
@@ -39,6 +39,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [isTransforming, setIsTransforming] = useState(false)
   const [transformationConfig, setTransformationConfig] = useState(config)
+  const [isPending, startTransition] = useTransition()
 
   const initialValues = data && action === 'Update' ? {
     title: data?.title,
@@ -66,7 +67,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
     const imageSize = aspectRatioOptions[Value as AspectRatioKey]
     setImage((prevState: any) => ({
       ...prevState,
-      aspectRatio: imageSize.aspectRatio,
+      aspectRatio: imageSize.aspectRatio,   // in the tutorial aspectRation
       width: imageSize.width,
       height: imageSize.height
     }))
@@ -90,8 +91,15 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
   }
 
 
-  const onTransformHandler = () => {
-
+  const onTransformHandler = async () => {
+    setIsTransforming(true)
+    setTransformationConfig(
+      deepMergeObjects(newTransformation, transformationConfig)
+    )
+    setNewTransformation(null)
+    startTransition(async () => {
+      // await updateCredits(userId, creditFee)
+    })
   }
 
 
