748d947c5420648232d9127c2bf697afb5ef2477
Vikash Thakur <thakvikash622@gmail.com>
2024-08-22 12:16:46 +0530

    Update the code

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,9 @@
+import Header from '@/components/shared/Header'
 import React from 'react'
 
 const AddTransformationTypePage = () => {
   return (
-    <div>AddTransformationTypePage</div>
+    <Header title="Transformation page" subtitle="this is subtitle of the page" />
   )
 }
 
---
+++ components/shared/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = ({ title, subtitle}: {title: string, subtitle?: string}) => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
