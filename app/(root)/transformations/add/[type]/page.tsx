1ded2b5e97c88452fa45daec3050459c7bec463d
Vikash Thakur <thakvikash622@gmail.com>
2024-08-22 15:35:39 +0530

    Working on transformation page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,15 +1,20 @@
 import Header from '@/components/shared/Header'
 import React from 'react'
-import {transformationTypes} from "@/constants"
+import { transformationTypes } from "@/constants"
+import TransformationForm from '@/components/shared/TransformationForm'
 
-const AddTransformationTypePage = ({ params: {type}}: SearchParamProps) => {
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
 
   const Transformation = transformationTypes[type]
   return (
-    <Header
-      title={Transformation.title}
-      subtitle={Transformation.subTitle}
-    />
+    <>
+      <Header
+        title={Transformation.title}
+        subtitle={Transformation.subTitle}
+      />
+
+      <TransformationForm />
+    </>
   )
 }
 
