91845647b9fe0be9baf50cdfeb3643309c163283
Vikash Thakur <thakvikash622@gmail.com>
2024-08-26 21:51:54 +0530

    Update the auth component

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,13 @@ import { SignUp } from '@clerk/nextjs'
 import React from 'react'
 
 const SignUpPage = () => {
-  return <SignUp afterSignOutUrl="/" />
+  console.log(process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL); // Should output '/sign-up'
+  console.log(process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL); // Should output '/'
+
+  return <SignUp
+    forceRedirectUrl="/credits"
+  />
+
 }
 
 export default SignUpPage
